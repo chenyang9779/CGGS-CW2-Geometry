435249b78316a3fbe6bde5251315c78101199966
Chenyang Wu <aaronwu608@gmail.com>
2025-03-21 21:43:55 +0000

    Add comment for sec4

--- code/Section4/main.cpp
+++ code/Section4/main.cpp
@@ -68,8 +68,11 @@ void computePrincipalCurvatures(const Mesh &mesh, vector<Vector2d> &curvatures, 
 
     for (size_t i = 0; i < n; ++i)
     {   
+        // Current vertex position & normal
         const Vector3d &v = mesh.vertices[i];
         const Vector3d &nm = mesh.normals[i];
+
+        // Construct local frame R with x_axis, y_axis in tangent plane, nm as z-axis
         Vector3d x_axis = nm.cross(Vector3d::UnitY());
 
         x_axis = nm.cross(Vector3d::UnitX());
@@ -80,6 +83,8 @@ void computePrincipalCurvatures(const Mesh &mesh, vector<Vector2d> &curvatures, 
         R.col(0) = x_axis;
         R.col(1) = y_axis;
         R.col(2) = nm;
+
+        // Collect 1-ring neighbor positions
         unordered_set<int> neighborIndices;
         for (const auto &f : mesh.faces)
         {
@@ -91,6 +96,8 @@ void computePrincipalCurvatures(const Mesh &mesh, vector<Vector2d> &curvatures, 
             }
         }
         neighborIndices.erase(i);
+
+        // Build the system A * coeffs = b
         vector<Vector3d> neighbors;
         for (int idx : neighborIndices)
         {
@@ -100,23 +107,34 @@ void computePrincipalCurvatures(const Mesh &mesh, vector<Vector2d> &curvatures, 
         VectorXd b(neighbors.size());
         for (size_t j = 0; j < neighbors.size(); ++j)
         {
+            // Local coords relative to v
             Vector3d p_local = R.transpose() * (neighbors[j] - v);
             double x = p_local.x();
             double y = p_local.y();
             double z = p_local.z();
+
+            // Fill row in A and b
             A.row(j) << x * x, y * y, x * y, x, y, 1.0;
             b(j) = z;
         }
+
+        // Solve via SVD
         JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
         VectorXd coeffs = svd.solve(b);
+
+        // Hessian
         Matrix2d H;
         H << 2.0 * coeffs(0), coeffs(2), coeffs(2), 2.0 * coeffs(1);
+
+        // Diagonalize H
         SelfAdjointEigenSolver<Matrix2d> es(H);
         curvatures[i] = es.eigenvalues();
 
+        // Rotate eigenvectors back to global
         Eigen::Vector2d e1 = es.eigenvectors().col(0);
         Eigen::Vector2d e2 = es.eigenvectors().col(1);
 
+        // Construct global directions
         dir1[i] = e1(0) * x_axis + e1(1) * y_axis;
         dir2[i] = e2(0) * x_axis + e2(1) * y_axis;
     }
