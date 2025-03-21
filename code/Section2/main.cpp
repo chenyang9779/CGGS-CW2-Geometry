f2a33b6809435e0adc368fd30b379222bd9dd31b
Chenyang Wu <aaronwu608@gmail.com>
2025-03-21 20:40:21 +0000

    Add param to sec2,3

--- code/Section2/main.cpp
+++ code/Section2/main.cpp
@@ -16,16 +16,17 @@
 #include "mean_curvature_flow.h"
 #include <chrono>
 #include <filesystem>
-
+#include <cstdlib>
+#include <string>
 
 using namespace Eigen;
 using namespace std;
 
-bool isFlowing = false;
-bool isExplicit = true;
-double timeStepRatio = 0.5;
+// Global parameters with default values:
+bool isFlowing;
+bool isExplicit;
+double timeStepRatio;
 double timeStep;
-
 
 polyscope::SurfaceMesh* psMesh;
 
@@ -35,57 +36,58 @@ MatrixXd origV, currV, Hn, faceNormals;
 SparseMatrix<double> d0, W, M, MInv, L;
 VectorXd vorAreas, H, faceAreas;
 
-
 void callback_function() {
     ImGui::PushItemWidth(50);
-    
     ImGui::TextUnformatted("Flow Parameters");
     ImGui::Separator();
-    bool changed = ImGui::Checkbox("Flow", &isFlowing);
+    ImGui::Checkbox("Flow", &isFlowing);
     ImGui::PopItemWidth();
     if (!isFlowing)
         return;
     
     mean_curvature_flow(F, L, timeStep, M, MInv, boundVMask, isExplicit, currV);
-    
     psMesh->updateVertexPositions(currV);
 }
 
-
-int main()
+int main(int argc, char** argv)
 {
-    readOFF(DATA_PATH "/lion-head.off",origV, F);
-    currV = origV.rowwise()-origV.colwise().mean();
+    if (argc != 5 ) {
+        cerr << "Usage: " << argv[0] << "isFlowing isExplicit timeStepRatio <mesh.off>" << endl;
+        return 1;
+    }
+
+    string arg1(argv[1]);
+    isFlowing = (arg1 == "true" || arg1 == "1");
+    string arg2(argv[2]);
+    timeStepRatio = atof(argv[3]);
+    isExplicit = (arg2 == "true" || arg2 == "1");
+
+    readOFF(argv[4], origV, F);
+    currV = origV.rowwise() - origV.colwise().mean();
     create_edge_list(F, E, EF, boundEMask, boundVMask, boundVertices);
     
     polyscope::init();
     psMesh = polyscope::registerSurfaceMesh("Mesh", currV, F);
     compute_areas_normals(origV, F, faceAreas, faceNormals);
-    timeStep = timeStepRatio*faceAreas.minCoeff();
+    timeStep = timeStepRatio * faceAreas.minCoeff();
     
     compute_laplacian(origV, F, E, EF, boundEMask, d0, W, vorAreas);
-    L = d0.transpose()*W*d0;
+    L = d0.transpose() * W * d0;
     vector<Triplet<double>> MTris, MInvTris;
-    for (int i=0;i<vorAreas.size();i++){
-        MTris.push_back(Triplet<double>(i,i,vorAreas(i)));
-        MInvTris.push_back(Triplet<double>(i,i,1.0/vorAreas(i)));
+    for (int i = 0; i < vorAreas.size(); i++){
+        MTris.push_back(Triplet<double>(i, i, vorAreas(i)));
+        MInvTris.push_back(Triplet<double>(i, i, 1.0 / vorAreas(i)));
     }
     M.resize(vorAreas.size(), vorAreas.size());
     MInv.resize(vorAreas.size(), vorAreas.size());
     M.setFromTriplets(MTris.begin(), MTris.end());
     MInv.setFromTriplets(MInvTris.begin(), MInvTris.end());
     
-    
-    
     /*psMesh->addVertexScalarQuantity("Gaussian Curvature", G.array()/vorAreas.array())->setEnabled(true);
     psMesh->addVertexScalarQuantity("Gaussian Regions", G.unaryExpr([](double x) { return (x > 0) - (x < 0); }));
     psMesh->addVertexScalarQuantity("Mean Curvature", H);
     psMesh->addVertexVectorQuantity("Mean Curvature Normal", Hn);*/
     
-    
     polyscope::state::userCallback = callback_function;
-    
     polyscope::show();
-    
 }
-
--- code/Section3/main.cpp
+++ code/Section3/main.cpp
@@ -15,7 +15,6 @@
 #include <chrono>
 #include <filesystem>
 
-
 using namespace Eigen;
 using namespace std;
 
@@ -26,48 +25,52 @@ SparseMatrix<double> d0, W;
 VectorXd vorAreas;
 MatrixXd UVBound, UV;
 
-
-
-int main()
+int main(int argc, char **argv)
 {
-    readOFF(DATA_PATH "/param/gargoyle2.off",V, F);
+
+    if (argc < 2)
+    {
+        cerr << "Usage: " << argv[0] << " <mesh.off>" << endl;
+        return 1;
+    }
+    readOFF(argv[1], V, F);
     create_edge_list(F, E, EF, boundEMask, boundVMask, boundVertices, true);
-    
+
     polyscope::init();
-    polyscope::SurfaceMesh* psMesh = polyscope::registerSurfaceMesh("Original Mesh", V, F);
+    polyscope::SurfaceMesh *psMesh = polyscope::registerSurfaceMesh("Original Mesh", V, F);
     compute_laplacian(V, F, E, EF, boundEMask, d0, W, vorAreas);
-    double r = sqrt(vorAreas.sum()/M_PI);
-    
+    double r = sqrt(vorAreas.sum() / M_PI);
+
     auto start = std::chrono::high_resolution_clock::now();
     UVBound = compute_boundary_embedding(V, boundVertices, r);
     UV = compute_tutte_embedding(boundVertices, UVBound, d0, W);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Tutte Parameterization took " << (double)(duration.count())/1000.0 << " seconds to execute." << std::endl;
-    
-    MatrixXd UV3D = MatrixXd::Zero(UV.rows(), 3);
-    UV3D.block(0,0,UV.rows(), 1) = UV.col(0);
-    UV3D.block(0,2,UV.rows(), 1) = UV.col(1);
+    std::cout << "Tutte Parameterization took " << (double)(duration.count()) / 1000.0 << " seconds to execute." << std::endl;
 
-    polyscope::SurfaceMesh* psParam = polyscope::registerSurfaceMesh("Parameterization Mesh", UV3D, F)->setEdgeWidth(1.0);
-    psMesh->addVertexParameterizationQuantity("UV Mapping", UV)->setCheckerSize(r/20.0)->setEnabled(true);
-    
+    MatrixXd UV3D = MatrixXd::Zero(UV.rows(), 3);
+    UV3D.block(0, 0, UV.rows(), 1) = UV.col(0);
+    UV3D.block(0, 2, UV.rows(), 1) = UV.col(1);
+
+    polyscope::SurfaceMesh *psParam = polyscope::registerSurfaceMesh("Parameterization Mesh", UV3D, F)->setEdgeWidth(1.0);
+    psMesh->addVertexParameterizationQuantity("UV Mapping", UV)->setCheckerSize(r / 20.0)->setEnabled(true);
+
     vector<RowVector3d> boundNodes;
     vector<RowVector2i> boundEdges;
-    
-    for (int i=0;i<E.rows();i++){
+
+    for (int i = 0; i < E.rows(); i++)
+    {
         if (!boundEMask(i))
             continue;
-        
-        boundNodes.push_back(V.row(E(i,0)));
-        boundNodes.push_back(V.row(E(i,1)));
-        RowVector2i boundEdge; boundEdge<<boundNodes.size()-2, boundNodes.size()-1;
+
+        boundNodes.push_back(V.row(E(i, 0)));
+        boundNodes.push_back(V.row(E(i, 1)));
+        RowVector2i boundEdge;
+        boundEdge << boundNodes.size() - 2, boundNodes.size() - 1;
         boundEdges.push_back(boundEdge);
     }
-    
-    polyscope::registerCurveNetwork("Seam", boundNodes, boundEdges)->setRadius(0.0015)->setColor(glm::vec3{0.0,0.0,1.0});
-    
-    polyscope::show();
-    
-}
 
+    polyscope::registerCurveNetwork("Seam", boundNodes, boundEdges)->setRadius(0.0015)->setColor(glm::vec3{0.0, 0.0, 1.0});
+
+    polyscope::show();
+}
