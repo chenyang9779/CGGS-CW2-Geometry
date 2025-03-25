b61e1e4508c04df7af8b500924852a4d40f850d7
Chenyang Wu <aaronwu608@gmail.com>
2025-03-25 18:36:59 +0000

    update submit file

--- prepare_submit.sh
+++ prepare_submit.sh
@@ -5,10 +5,6 @@ cd /home/chenyang/repos/CGGS-CW2-Geometry
 mv ~/Downloads/CGGS_cw2_Geometry.pdf s2693586/s2693586.pdf
 
 
-cp code/include/compute_laplacian.h code/include/compute_mean_curvature_normal.h code/include/compute_angle_defect.h s2693586/section1/
-
-cp code/include/compute_mean_curvature_normal.h code/include/compute_angle_defect.h code/include/compute_laplacian.h code/include/mean_curvature_flow.h s2693586/section2/
-
-cp code/include/tutte_parameterization.h code/include/compute_laplacian.h s2693586/section3/
+cp code/include/compute_laplacian.h code/include/compute_mean_curvature_normal.h code/include/compute_angle_defect.h code/include/mean_curvature_flow.h code/include/tutte_parameterization.h s2693586/section123/
 
 cp -r code/Section4 s2693586
